6d0940c063fa930357508df621bd6fc9adf2ca3d
Braytiner <braytiner@hotmail.com>
2021-06-20 11:14:15 -0300

    Remoção da vírgula em comentário

--- CleanArchitecture.Infra.Dados/Contextos/AplicacaoBDContextoFabrica.cs
+++ CleanArchitecture.Infra.Dados/Contextos/AplicacaoBDContextoFabrica.cs
@@ -8,7 +8,7 @@ namespace CleanArchitecture.Infra.Dados.Contextos
   {
     /*
      * Vamos ter que criar o AplicacaoDbContexto em tempo de projeto para gerar as migrações. Para isso, 
-     * implementamos a interface IDesignTimeDbContextFactory<TContext>, pois, por convenção, se uma 
+     * implementamos a interface IDesignTimeDbContextFactory<TContext>, pois por convenção, se uma 
      * classe que implementa esta interface for encontrada no mesmo projeto que o DbContext ou no 
      * projeto de inicialização do aplicativo, as ferramentas contornam as outras maneiras de criar o 
      * DbContext e usam a fábrica em tempo de projeto no seu lugar.
